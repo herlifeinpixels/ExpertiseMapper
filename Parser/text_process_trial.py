bb239c96591e5299519069f3c00bdc090175ec16
Hannah <yw.hannah@gmail.com>
2013-03-30 21:15:16 -0400

    cleam up json data

--- Parser/text_process_trial.py
+++ Parser/text_process_trial.py
@@ -1,49 +1,60 @@
 import os
 import json
 import re
+from collections import defaultdict
 from pprint import pprint
- 
-# number, "user_skill" : "
-#  			{"prof" : {*}, "skill" : {*, "years": {*}},
-#				{"prof" : {*}, "skill" : {*, "years": {*}},
-#				{"prof" : {*}, "skill" : {*, "years": {*}"
- 
-# we want:
 	
- 
-def main():
+curr_dir = 'C:\Users\Hannah\Documents\GitHub\ExpertiseMapper'
+
+def main()  :
         # TODO catch IO errors
         raw = readfile()
-        f = open('Failed.txt', 'w')
-        json.dumps(raw, f)
-        f.close()
-        print raw;
-        print "";
+        #print json.loads(raw[0][0])["user_id"]
         
+        # TODO: Save backup
+        # f = open('backup.txt', 'w')
+        # f.close()
+        
+        # Build skills repository
+        allSkills = getAllSkills(raw)
+
+# Sort into dictionary of skills        
+def getAllSkills(raw) :
+    data = {}
+    # d = {'technology': {'importance': 1, 'users': [1 , 2]}, 
+    #            'java': {'importance': 3, 'users': [4}}
+    
+    return data
+
+# Parse JSON file
 def readfile() :
         # ----------- SQL Query to list skills from each user --------------
         # SELECT `user_id`, GROUP_CONCAT(DISTINCT source_json ORDER BY source_json DESC SEPARATOR ', ')
         # FROM main_skill
         # GROUP BY `user_id`
         # ------------------------------------------------------------------
-        data = []
+        data = defaultdict(list)
         user = "user_id"
         skill = "user_skill"
-        curr_dir = 'C:\GitHub\ExpertiseMapper'
+        i = 0
         
+        # Validate 
         with open(curr_dir + '\Parser\main_skill50.json', 'r') as source:
-                for line in source:
-                        if line.find(user) != -1:
-                                #get user id
-                                id = line.rpartition(':')[2]
-                                print id 
-                        elif line.find(skill) != -1:
-                                # we get line with user skills
-                                # we want {'skill': 'Adobe', 'proficiency': 'beginner'}
-                                parts = line.split();
-                                parts = line.replace("\\", "")
-                                print parts
-                source.close()
+            for line in source:
+                if line.find(user) != -1:
+                    #get user id
+                    id = "{" + line
+                elif line.find(skill) != -1:
+                    #Clean up escapes
+                    #Replace "" with []s
+                    parts = line.replace("\\", "")
+                    parts = parts[0:14] + " [" + parts[16:]
+                    parts = parts[0:len(parts)-2] + "}]}"
+                    parts = id + parts
+                    #print parts
+                    data[i].append(parts)
+                    i = i + 1
+            source.close()
         
         return data
  
