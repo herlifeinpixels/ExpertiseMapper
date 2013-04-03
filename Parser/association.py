3f3bf7ec95704023ef53b968b982ad6ffd1c2c5f
Hannah Wei <yw.hannah@gmail.com>
2013-04-03 14:30:28 -0400

    apply association rules using the Python Orange lib

---
+++ Parser/association.py
@@ -0,0 +1,40 @@
+import os, json, re, orange, Orange
+from textProcessing import Parser
+
+from collections import defaultdict
+
+def main() :
+
+    dir = 'C:\\GitHub\\ExpertiseMapper\\Parser\\'
+    raw = 'skills500.json'
+    filename = 'skillTab.basket'
+    
+    os.chdir(dir)
+    skills = Parser(dir, raw)
+    
+    #populateTable(dir + filename, skills)
+    data = orange.ExampleTable(filename)
+    
+    # Apply association rules
+    rules = orange.AssociationRulesSparseInducer(data, support = 0.015)
+    
+    print "%5s   %5s" % ("supp", "conf")
+    for r in rules:
+        print "%5.3f   %5.3f   %s" % (r.support, r.confidence, r)
+    
+    #makes weighted edges between
+    generateEdges(rules)
+
+def populateTable(dir, s) :
+    #save as tab
+    with open(dir, 'a') as file:
+        
+        for i, key in enumerate(s.userSkills) :
+            #u = ', '.join(sum((i.split() for i in s.userSkills[key]), [])) 
+            u = ', '.join(s.userSkills[key])
+            if (i+1) == len(s.userSkills):
+                file.write(u)
+            else:
+                file.write(u + '\n')
+                
+main()
